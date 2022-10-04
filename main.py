a10711e1b635c71e7084ea73bfdda9241be9420c
Andrew Robson <robson.andrew@gmail.com>
2022-10-04 20:17:44 +1300

    Generate list from dictionary

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     val = read_file("./example.csv")
-    return val
+
+    res = []
+    for key, value in val.items():
+        iter = int(value[0])
+        for x in range (0, iter):
+            res.append(key)
+    return res
